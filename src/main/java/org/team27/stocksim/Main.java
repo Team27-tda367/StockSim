3214416bee4e1b78596b76622aed2a2930ae33b3
olleac@chalmers.se <olle.ackebjer@gmail.com>
2025-11-17 14:55:48 +0100

    Added structure over FXML, css, controllers and db

--- src/main/java/org/team27/stocksim/Main.java
+++ src/main/java/org/team27/stocksim/Main.java
@@ -1,9 +1,40 @@
 package org.team27.stocksim;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, StockSim!");
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.team27.stocksim.db.Database;
 
+public class Main extends Application {
+
+    @Override
+    public void init() throws Exception {
+        super.init();
+        // Starta databasen innan UI
+        Database.init();
     }
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource("/org/team27/stocksim/view/exampel.fxml"));
+
+        Scene scene = new Scene(loader.load(), 400, 300);
+
+        primaryStage.setTitle("Stocksim");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    @Override
+    public void stop() throws Exception {
+        // Stäng databasen snyggt
+        Database.close();
+        super.stop();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
---
+++ src/main/java/org/team27/stocksim/db/Database.java
@@ -0,0 +1,75 @@
+package org.team27.stocksim.db;
+
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.DatabaseTable;
+import com.j256.ormlite.table.TableUtils;
+
+import java.sql.SQLException;
+
+public class Database {
+
+    private static final String DATABASE_URL = "jdbc:sqlite:app.db";
+
+    private static ConnectionSource connectionSource;
+    private static Dao<Placeholder, Integer> placeholderDao;
+
+    public static void init() throws SQLException {
+        if (connectionSource != null) {
+            return;
+        }
+
+        try {
+            // Ladda SQLite-drivrutinen
+            Class.forName("org.sqlite.JDBC");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Kunde inte ladda SQLite-drivrutin", e);
+        }
+
+        connectionSource = new JdbcConnectionSource(DATABASE_URL);
+
+        // Skapa placeholder-tabell om den inte finns
+        TableUtils.createTableIfNotExists(connectionSource, Placeholder.class);
+
+        // Skapa DAO
+        placeholderDao = DaoManager.createDao(connectionSource, Placeholder.class);
+    }
+
+    public static Dao<Placeholder, Integer> getPlaceholderDao() throws SQLException {
+        if (placeholderDao == null) {
+            init();
+        }
+        return placeholderDao;
+    }
+
+    public static void close() {
+        if (connectionSource != null) {
+            try {
+                connectionSource.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                connectionSource = null;
+                placeholderDao = null;
+            }
+        }
+    }
+
+    /**
+     * Minimal placeholder-entitet som ORMLite kräver.
+     * Finns bara för att hålla ORM-koden levande tills riktiga modeller skapas.
+     */
+    @DatabaseTable(tableName = "placeholder")
+    public static class Placeholder {
+
+        @DatabaseField(generatedId = true)
+        private int id;
+
+        public Placeholder() {
+            // ORMLite kräver en tom konstruktor
+        }
+    }
+}
